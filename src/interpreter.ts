777a8d8227214ad538af0527fd1b4d5add8929b1
Ryan McDermott <ryantmcdermott@gmail.com>
2016-09-19 21:21:57 -0700

    Add lexer

--- src/interpreter.ts
+++ src/interpreter.ts
@@ -11,14 +11,6 @@ interface Token {
 }
 
 var TOKEN_EXPRESSIONS: Array<LanguageToken> = [
-    {
-        pattern: /[\n\t]+/,
-		tag: null
-    },
-	{
-		pattern: /#[^\n]*/,
-		tag: null
-	},
 	{
 		pattern: /\:=/,
 		tag: 'RESERVED',
@@ -111,32 +103,150 @@ var TOKEN_EXPRESSIONS: Array<LanguageToken> = [
 		pattern: /[A-Za-z][A-Za-z0-9_]*/,
 		tag: 'ID',
 	},
+	{
+		pattern: /\s+/,
+		tag: 'SKIP'
+	},	
+    {
+        pattern: /[\n\t]+/,
+		tag: 'SKIP'
+    },
+	{
+		pattern: /#[^\n]*/,
+		tag: 'SKIP'
+	},
+	{
+		pattern: /$/,
+		tag: 'EOF'
+	},
 ]
 
-function lexer(source: String, languageTokens: Array<LanguageToken>) {
-	let pieces = source.split(' ');
-	let tokens: Array<Token> = [];
-	pieces.forEach((piece) => {
-		var token: Token;
-		for (var i=0; i < languageTokens.length; i++) {
-			if (languageTokens[i].pattern.exec(piece)) {
-				token = {
-					data: piece,
-					tag: languageTokens[i].tag
-				};
-			}
-		}
+function McState() {
 
-		if (!token) {
-			throw new Error('Token not recognized');
-		}
+ var that = this ;
 
-		tokens.push(token);
-	});
+ var rules = [] ;
+ 
+ var state = function (regex) {
+  return function (action) {
+   rules.push(new McRule(regex,action)) ;
+  } ;
+ } ;
 
-	console.warn(tokens);
+ state.rules = rules ;
+
+ state.lex = function (input) {
+   var nextCont = state.run(input) ;
+   while (typeof nextCont == "function") {
+     nextCont = nextCont() ;
+   }
+   return nextCont ;
+ }
+
+ state.run = McRunMethod ;
+
+ state.continuation = function (input) {
+   return function () {
+     return state.run(input) ;
+   } ;
+ }
+
+ return state ;
 }
 
-lexer("x := 1; \n x + 2", TOKEN_EXPRESSIONS);
+
+function McRule(regex,action) {
+  // Each rule is re-written to match prefixes of the input string.
+  this.regex = new RegExp("^(" + regex.source + ")") ;
+  if (this.regex.compile) this.regex.compile(this.regex) ;
+  this.action = action ;
+}
+
+McRule.prototype.matches = function (s) {
+ var m = s.match(this.regex) ;
+ if (m) m.shift() ;
+ return m ;
+} ;
+
+
+function McRunMethod(input) {
+  var longestMatchedRule = null ;
+  var longestMatch = null ;
+  var longestMatchedLength = -1 ;
+  
+  for (var i = this.rules.length-1; i >= 0; --i) {
+    var r = this.rules[i] ;
+
+    var m = r.matches(input) ;
+    
+    if (m && (m[0].length >= longestMatchedLength)) {
+      longestMatchedRule = r ;
+      longestMatch = m ;
+      longestMatchedLength = m[0].length ;
+    }
+  }
+  
+  if (longestMatchedRule) {
+    return longestMatchedRule.action(longestMatch,input.substring(longestMatchedLength),this) ;
+  } else {
+    throw ("Lexing error; no match found for: '" + input + "'") ;
+  }
+}
+
+/* Creates a continuation that switches analysis to another lexical state.  */
+function McCONTINUE(state) {
+  return function (rest) {
+    return state.run(rest) ;
+  }
+}
+
+
+var McLexer = {
+ State : McState 
+} ;
+
+
+
+function lexer(code : string, languageRegexes) {
+	var INIT = new McLexer.State() ;
+
+	var tokens = [] ;
+
+	languageRegexes.forEach((regex) => {
+		INIT(regex.pattern)(function (match, rest, state) {
+			console.warn(match);
+			console.warn(state);
+			switch(regex.tag) {
+				case 'ID':
+				case 'INT':
+				case 'RESERVED':
+					tokens.push({
+						data: match[0],
+						tag: regex.tag
+					});
+
+					return state.continuation(rest) ;
+					break;
+
+				case 'SKIP':
+					McCONTINUE(INIT);
+					break;
+
+				case 'EOF':
+					return null;
+					break;
+			}
+		});
+	});
+
+	INIT.lex(code);
+
+	return tokens
+}
+
+
+var tokens = lexer("x:=1;", TOKEN_EXPRESSIONS);
+
+console.warn(tokens);
 
 
